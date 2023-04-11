0508eec92e3b14186bce2868fc3583b518870228
eStrawe <ambroise59.linquette@gmail.com>
2023-04-12 00:26:02 +0200

    modif génération cible

--- custom.js
+++ custom.js
@@ -17,7 +17,8 @@ function Start() {
     document.getElementById("no-ammo").style.display = 'none';
     document.getElementById("currentScore").innerHTML = count;
     document.getElementById("currentAmmo").innerHTML = bullets;
-    IntervId = setInterval(createDuck, 2000);
+    IntervId = setInterval(function() { createBird('duck') }, 2000);
+    IntervId = setInterval(function() { createBird('hummingbird') }, 5000);
 
 }
 
@@ -26,15 +27,6 @@ function End() {
     clearInterval(IntervId);
     start = false;
 }
-
-function shoot(bird){
-    
-    if (bullets != 0){
-    bird.style.display= 'none';
-    document.getElementById("currentScore").innerHTML = ++count;
-    }
-}
-
 
 window.onclick = function() { 
     if(start != false){
@@ -49,76 +41,80 @@ window.onclick = function() {
 }
 
 
-const duckSize = 40;
+const birdSize = 40;
+const hummingbirdSize = 60;
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
 // Fonction pour créer un canard de façon aléatoire
 
-function createDuck() {
-    let duck = document.createElement('img');
-    duck.style.position = 'absolute';
-    duck.style.width = duckSize + 'px';
-    duck.style.height = duckSize + 'px';
-    duck.style.transition='img/duck.gif';
-    duck.setAttribute("src", "img/duck.gif");
-
-    
-    duck.style.top = Math.floor(Math.random() * (windowHeight - duckSize)) + 'px';
-
+function createBird(type) {
+    let bird = document.createElement('img');
+    bird.style.position = 'absolute';
+    bird.style.width = birdSize + 'px';
+    bird.style.height = birdSize + 'px';
+    bird.style.top = Math.floor(Math.random() * (windowHeight - birdSize)) + 'px';
+    switch (type) {
+        case 'duck': 
+        bird.setAttribute("src", "img/duck.gif");
+        break;
+        case 'hummingbird': 
+        bird.setAttribute("src", "img/flutter.gif");
+        break;
+    }
+    let speed = Math.floor(Math.random() * 5) + 1;
     // Déterminer la direction de l'apparition du canard (de gauche à droite ou de droite à gauche)
     let direction = Math.floor(Math.random() * 2);
     if (direction === 0) { // de gauche à droite
         
-        let speed = Math.floor(Math.random() * 5) + 1;
-        moveSquareRight(duck, speed);
+        moveSquareRight(bird, speed);
     } else { // de droite à gauche
-        duck.style.transform='scaleX(-1)';
-        let speed = Math.floor(Math.random() * 5) + 1;
-        moveSquareLeft(duck, speed);
+        bird.style.transform='scaleX(-1)';
+
+        moveSquareLeft(bird, speed);
     }
 
     // Ajouter l'événement de clic pour supprimer le canard
-    duck.addEventListener('click', function() {
+    bird.addEventListener('click', function() {
         if (bullets != 0){
-        console.log(count);
+        killBird(bird, "UserKill")
         document.getElementById("currentScore").innerHTML = ++count;
-        killDuck(duck, "UserKill")
         } // Supprimer le canard
     }, {once: true});
 
     // Ajouter le canard à la page web
-    document.body.appendChild(duck);
+    document.body.appendChild(bird);
 }
 
 // Fonction pour faire bouger le canard de gauche à droite
-function moveSquareRight(duck, speed) {
-    let position = -duckSize;
+function moveSquareRight(bird, speed) {
+    let position = -birdSize;
     let interval = setInterval(function () {
         position += speed;
-        duck.style.left = position + 'px';
+        bird.style.left = position + 'px';
         if (position > windowWidth) {
             clearInterval(interval);
-            killDuck(duck, "left");
+            killBird(bird, "left");
         }
     }, 20);
 }
 
 // Fonction pour faire bouger le canard de droite à gauche
-function moveSquareLeft(duck, speed) {
+function moveSquareLeft(bird, speed) {
     let position = windowWidth;
     let interval = setInterval(function () {
         position -= speed;
-        duck.style.left = position + 'px';
-        if (position < -duckSize) {
+        bird.style.left = position + 'px';
+        if (position < -birdSize) {
             clearInterval(interval);
-            killDuck(duck, "right");
+            killBird(bird, "right");
         }
     }, 20);
 }
 
 // Fonction pour supprimer le canard
-function killDuck(duck, msg) {
-    duck.remove();
-        console.log('KillDuck ' + msg);
+function killBird(bird, msg) {
+
+    bird.remove();
+      
 }
