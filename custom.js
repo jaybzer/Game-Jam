047a7dc80a1b5c59738a2096f9a8ae6b17293759
eStrawe <ambroise59.linquette@gmail.com>
2023-04-11 17:28:04 +0200

    ajout fonction start

--- custom.js
+++ custom.js
@@ -1,14 +1,27 @@
 let bullets = 5; 
 let count = 0; 
-
-
+let start = false; 
+let nbBirds = 3;
 
 window.onload = function() {
     document.getElementById("currentScore").innerHTML = count;
     document.getElementById("currentAmmo").innerHTML = bullets;
   };
 
+function Start() {
+    start = true;
+    bullets = 5;
+    count = 0;
+    bullets++;
+    document.getElementById("play").style.display = 'none';
+    document.getElementById("currentScore").innerHTML = count;
+    document.getElementById("currentAmmo").innerHTML = bullets;
+    for (let i = 1; i<nbBirds++; i++ ){
+        document.getElementById("b"+i).style.animationName = "bird"+i; 
+    }
 
+
+}
 
 function shoot(bird){
     
@@ -20,15 +33,20 @@ function shoot(bird){
 
 
 window.onclick = function() { 
-    console.log("yes");
-    if (bullets != 0){
-	bullets--; 
-    console.log(bullets);
-	document.getElementById("currentAmmo").innerHTML=bullets;
-    }
-    else {
-        console.log("yno");
-        alert("Vous n'avez plus de munitions");
+    if(start != false){
+        if (bullets != 0){
+	    bullets--; 
+	    document.getElementById("currentAmmo").innerHTML=bullets;
+        }
+        else {
+            alert("Vous n'avez plus de munitions");
+            document.getElementById("play").style.display = 'block';
+            for (let i = 1; i<nbBirds++; i++ ){
+                document.getElementById("b"+i).style.animationName = "none";
+                document.getElementById("b"+i).style.display = 'block';
+            }
+            
+        }
     }
 }
 
